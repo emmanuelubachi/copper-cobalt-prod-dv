f592dad39888cb8ba54784259257e842aa6ba8b9
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-06-03 00:46:15 +0100

    build: update page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,17 +8,17 @@ import MobileNav from "@/components/elements/mobileNav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="flex">
-        <aside className="flex flex-row">
+      <div className="flex bg-muted/40">
+        <aside>
           <Nav />
           <Drawer />
-          <MobileNav />
         </aside>
-        <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <MobileNav />
+        <div className="flex min-h-screen w-screen flex-col ">
           <div className="relative">
             <Header />
           </div>
-          <div className="flex flex-col">{children}</div>
+          {children}
         </div>
       </div>
     </>
