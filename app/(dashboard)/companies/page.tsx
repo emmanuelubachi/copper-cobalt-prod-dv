b49bf7fa2c4306faa92815f678b30ced27317e9f
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-06-03 18:55:50 +0100

    build: add page

--- app/(dashboard)/companies/page.tsx
+++ app/(dashboard)/companies/page.tsx
@@ -1,5 +1,10 @@
+import GridList from "@/components/sections/gridList";
 import React from "react";
 
 export default function Page() {
-  return <div></div>;
+  return (
+    <main className="mb-24 mt-10 p-4 sm:mb-20 sm:mt-16 sm:px-6 sm:py-4">
+      <GridList />
+    </main>
+  );
 }
