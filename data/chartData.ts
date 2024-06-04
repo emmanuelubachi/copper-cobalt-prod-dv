27a6cf8f75a856d9511d4725417161d906c02322
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-06-04 20:52:21 +0100

    build: add data

---
+++ data/chartData.ts
@@ -0,0 +1,293 @@
+import { DollarSign, Weight } from "lucide-react";
+
+export const kpiCard = [
+  {
+    title: "Quantities Copper (T)",
+    description: "+20.1% from last year. Quantities Copper (T)",
+    category: "copper",
+    value: "2.9M",
+    icon: Weight,
+  },
+  {
+    title: "Total Copper Transaction (USD)",
+    description: "+180.1% from last year, Total Copper Transaction (USD)",
+    category: "copper",
+    value: "$20.0bn",
+    icon: DollarSign,
+  },
+  {
+    title: "Quantities Copper (T)",
+    description: "+19% from last year, Quantities Copper (T)",
+    category: "cobalt",
+    value: "507.9K",
+    icon: Weight,
+  },
+  {
+    title: "Total Copper Transaction (USD)",
+    description: "+201 since last year, Total Copper Transaction (USD)",
+    category: "cobalt",
+    value: "$8.6bn",
+    icon: DollarSign,
+  },
+];
+
+export const exportQuantityData = [
+  {
+    date: "2010",
+    Copper: 2890,
+    Cobalt: 2338,
+  },
+  {
+    date: "2011",
+    Copper: 2756,
+    Cobalt: 2103,
+  },
+  {
+    date: "2012",
+    Copper: 3322,
+    Cobalt: 2194,
+  },
+  {
+    date: "2013",
+    Copper: 3470,
+    Cobalt: 2108,
+  },
+  {
+    date: "2014",
+    Copper: 3475,
+    Cobalt: 1812,
+  },
+  {
+    date: "2015",
+    Copper: 3129,
+    Cobalt: 1726,
+  },
+  {
+    date: "2016",
+    Copper: 3490,
+    Cobalt: 1982,
+  },
+  {
+    date: "2017",
+    Copper: 2903,
+    Cobalt: 2012,
+  },
+  {
+    date: "2018",
+    Copper: 2643,
+    Cobalt: 2342,
+  },
+  {
+    date: "2019",
+    Copper: 2837,
+    Cobalt: 2473,
+  },
+  {
+    date: "2020",
+    Copper: 2954,
+    Cobalt: 3848,
+  },
+  {
+    date: "2021",
+    Copper: 3239,
+    Cobalt: 3736,
+  },
+  {
+    date: "2022",
+    Copper: 3470,
+    Cobalt: 3859,
+  },
+  {
+    date: "2023",
+    Copper: 3475,
+    Cobalt: 3859,
+  },
+];
+
+export const exportTransactionData = [
+  {
+    date: "2010",
+    Copper: 25890,
+    Cobalt: 23638,
+  },
+  {
+    date: "2011",
+    Copper: 28756,
+    Cobalt: 29103,
+  },
+  {
+    date: "2012",
+    Copper: 33322,
+    Cobalt: 22194,
+  },
+  {
+    date: "2013",
+    Copper: 33470,
+    Cobalt: 24108,
+  },
+  {
+    date: "2014",
+    Copper: 36475,
+    Cobalt: 18812,
+  },
+  {
+    date: "2015",
+    Copper: 34129,
+    Cobalt: 19726,
+  },
+  {
+    date: "2016",
+    Copper: 36490,
+    Cobalt: 17982,
+  },
+  {
+    date: "2017",
+    Copper: 23903,
+    Cobalt: 24012,
+  },
+  {
+    date: "2018",
+    Copper: 25643,
+    Cobalt: 26342,
+  },
+  {
+    date: "2019",
+    Copper: 22837,
+    Cobalt: 27473,
+  },
+  {
+    date: "2020",
+    Copper: 28954,
+    Cobalt: 34848,
+  },
+  {
+    date: "2021",
+    Copper: 31239,
+    Cobalt: 31736,
+  },
+  {
+    date: "2022",
+    Copper: 32470,
+    Cobalt: 33859,
+  },
+  {
+    date: "2023",
+    Copper: 34475,
+    Cobalt: 35859,
+  },
+];
+
+export const companyData = [
+  {
+    name: "Shituru Mining",
+    initials: "AS",
+    email: "a.stone@gmail.com",
+    textColor: "text-fuchsia-800 dark:text-fuchsia-500",
+    bgColor: "bg-fuchsia-100 dark:bg-fuchsia-500/20",
+    href: "#",
+  },
+  {
+    name: "Anvil Mining",
+    initials: "EB",
+    email: "e.bern@gmail.com",
+    textColor: "text-blue-800 dark:text-blue-500",
+    bgColor: "bg-blue-100 dark:bg-blue-500/20",
+    href: "#",
+  },
+  {
+    name: "MMG Kinsevere",
+    initials: "AM",
+    email: "a.flow@acme.com",
+    textColor: "text-pink-800 dark:text-pink-500",
+    bgColor: "bg-pink-100 dark:bg-pink-500/20",
+    href: "#",
+  },
+  {
+    name: "Mutanda Mining",
+    initials: "TP",
+    email: "t.palstein@acme.com",
+    textColor: "text-emerald-800 dark:text-emerald-500",
+    bgColor: "bg-emerald-100 dark:bg-emerald-500/20",
+    href: "#",
+  },
+  {
+    name: "Shituru Mining",
+    initials: "SJ",
+    email: "s.johnson@gmail.com",
+    textColor: "text-orange-800 dark:text-orange-500",
+    bgColor: "bg-orange-100 dark:bg-orange-500/20",
+    href: "#",
+  },
+  {
+    name: "Musonoi Project",
+    initials: "DS",
+    email: "d.smith@gmail.com",
+    textColor: "text-indigo-800 dark:text-indigo-500",
+    bgColor: "bg-indigo-100 dark:bg-indigo-500/20",
+    href: "#",
+  },
+  {
+    name: "Megan Brown",
+    initials: "MB",
+    email: "m.brown@gmail.com",
+    textColor: "text-yellow-800 dark:text-yellow-500",
+    bgColor: "bg-yellow-100 dark:bg-yellow-500/20",
+    href: "#",
+  },
+  {
+    name: "Shituru Mining",
+    initials: "AS",
+    email: "a.stone@gmail.com",
+    textColor: "text-fuchsia-800 dark:text-fuchsia-500",
+    bgColor: "bg-fuchsia-100 dark:bg-fuchsia-500/20",
+    href: "#",
+  },
+  {
+    name: "Anvil Mining",
+    initials: "EB",
+    email: "e.bern@gmail.com",
+    textColor: "text-blue-800 dark:text-blue-500",
+    bgColor: "bg-blue-100 dark:bg-blue-500/20",
+    href: "#",
+  },
+  {
+    name: "MMG Kinsevere",
+    initials: "AM",
+    email: "a.flow@acme.com",
+    textColor: "text-pink-800 dark:text-pink-500",
+    bgColor: "bg-pink-100 dark:bg-pink-500/20",
+    href: "#",
+  },
+  {
+    name: "Mutanda Mining",
+    initials: "TP",
+    email: "t.palstein@acme.com",
+    textColor: "text-emerald-800 dark:text-emerald-500",
+    bgColor: "bg-emerald-100 dark:bg-emerald-500/20",
+    href: "#",
+  },
+  {
+    name: "Shituru Mining",
+    initials: "SJ",
+    email: "s.johnson@gmail.com",
+    textColor: "text-orange-800 dark:text-orange-500",
+    bgColor: "bg-orange-100 dark:bg-orange-500/20",
+    href: "#",
+  },
+  {
+    name: "Musonoi Project",
+    initials: "DS",
+    email: "d.smith@gmail.com",
+    textColor: "text-indigo-800 dark:text-indigo-500",
+    bgColor: "bg-indigo-100 dark:bg-indigo-500/20",
+    href: "#",
+  },
+  {
+    name: "Megan Brown",
+    initials: "MB",
+    email: "m.brown@gmail.com",
+    textColor: "text-yellow-800 dark:text-yellow-500",
+    bgColor: "bg-yellow-100 dark:bg-yellow-500/20",
+    href: "#",
+  },
+];
