09493dba213cdf87a8f8e67335fb3d5dfae9278b
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-06-04 20:52:35 +0100

    build: update utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function classNames(...classes: string[]): string {
+  return classes.filter(Boolean).join(" ");
+}
+
+export function sliceData<T>(data: T[], count: number): T[] {
+  return data.slice(0, count);
 }
