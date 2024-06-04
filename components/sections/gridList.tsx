44aa3dbc442f99be08af2f8a274a9bca9c309fbb
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-06-04 20:52:07 +0100

    build: modify components

--- components/sections/gridList.tsx
+++ components/sections/gridList.tsx
@@ -1,71 +1,14 @@
 // 'use client';
 import { ArrowUpRight } from "lucide-react";
 import { Card, Divider } from "@tremor/react";
+import { companyData } from "@/data/chartData";
 
 function classNames(...classes: string[]): string {
   return classes.filter(Boolean).join(" ");
 }
 
-const data = [
-  {
-    name: "Shituru Mining",
-    initials: "AS",
-    email: "a.stone@gmail.com",
-    textColor: "text-fuchsia-800 dark:text-fuchsia-500",
-    bgColor: "bg-fuchsia-100 dark:bg-fuchsia-500/20",
-    href: "#",
-  },
-  {
-    name: "Anvil Mining",
-    initials: "EB",
-    email: "e.bern@gmail.com",
-    textColor: "text-blue-800 dark:text-blue-500",
-    bgColor: "bg-blue-100 dark:bg-blue-500/20",
-    href: "#",
-  },
-  {
-    name: "MMG Kinsevere",
-    initials: "AM",
-    email: "a.flow@acme.com",
-    textColor: "text-pink-800 dark:text-pink-500",
-    bgColor: "bg-pink-100 dark:bg-pink-500/20",
-    href: "#",
-  },
-  {
-    name: "Mutanda Mining",
-    initials: "TP",
-    email: "t.palstein@acme.com",
-    textColor: "text-emerald-800 dark:text-emerald-500",
-    bgColor: "bg-emerald-100 dark:bg-emerald-500/20",
-    href: "#",
-  },
-  {
-    name: "Shituru Mining",
-    initials: "SJ",
-    email: "s.johnson@gmail.com",
-    textColor: "text-orange-800 dark:text-orange-500",
-    bgColor: "bg-orange-100 dark:bg-orange-500/20",
-    href: "#",
-  },
-  {
-    name: "Musonoi Project",
-    initials: "DS",
-    email: "d.smith@gmail.com",
-    textColor: "text-indigo-800 dark:text-indigo-500",
-    bgColor: "bg-indigo-100 dark:bg-indigo-500/20",
-    href: "#",
-  },
-  {
-    name: "Megan Brown",
-    initials: "MB",
-    email: "m.brown@gmail.com",
-    textColor: "text-yellow-800 dark:text-yellow-500",
-    bgColor: "bg-yellow-100 dark:bg-yellow-500/20",
-    href: "#",
-  },
-];
-
 export default function GridList() {
+  const data = companyData;
   return (
     <>
       <div className="flex items-center space-x-2">
