854fb4ae454e1a377bae1d87a210b9d415f8f76a
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-06-04 14:20:02 +0100

    build: add data file

---
+++ data/chartdata.ts
@@ -0,0 +1,125 @@
+export const areachartdata = [
+  {
+    date: "Jan 23",
+    SolarPanels: 2890,
+    Inverters: 2338,
+  },
+  {
+    date: "Feb 23",
+    SolarPanels: 2756,
+    Inverters: 2103,
+  },
+  {
+    date: "Mar 23",
+    SolarPanels: 3322,
+    Inverters: 2194,
+  },
+  {
+    date: "Apr 23",
+    SolarPanels: 3470,
+    Inverters: 2108,
+  },
+  {
+    date: "May 23",
+    SolarPanels: 3475,
+    Inverters: 1812,
+  },
+  {
+    date: "Jun 23",
+    SolarPanels: 3129,
+    Inverters: 1726,
+  },
+  {
+    date: "Jul 23",
+    SolarPanels: 3490,
+    Inverters: 1982,
+  },
+  {
+    date: "Aug 23",
+    SolarPanels: 2903,
+    Inverters: 2012,
+  },
+  {
+    date: "Sep 23",
+    SolarPanels: 2643,
+    Inverters: 2342,
+  },
+  {
+    date: "Oct 23",
+    SolarPanels: 2837,
+    Inverters: 2473,
+  },
+  {
+    date: "Nov 23",
+    SolarPanels: 2954,
+    Inverters: 3848,
+  },
+  {
+    date: "Dec 23",
+    SolarPanels: 3239,
+    Inverters: 3736,
+  },
+];
+
+export const chartdata2 = [
+  {
+    date: "Jan 23",
+    2022: 45,
+    2023: 78,
+  },
+  {
+    date: "Feb 23",
+    2022: 52,
+    2023: 71,
+  },
+  {
+    date: "Mar 23",
+    2022: 48,
+    2023: 80,
+  },
+  {
+    date: "Apr 23",
+    2022: 61,
+    2023: 65,
+  },
+  {
+    date: "May 23",
+    2022: 55,
+    2023: 58,
+  },
+  {
+    date: "Jun 23",
+    2022: 67,
+    2023: 62,
+  },
+  {
+    date: "Jul 23",
+    2022: 60,
+    2023: 54,
+  },
+  {
+    date: "Aug 23",
+    2022: 72,
+    2023: 49,
+  },
+  {
+    date: "Sep 23",
+    2022: 65,
+    2023: 52,
+  },
+  {
+    date: "Oct 23",
+    2022: 68,
+    2023: null,
+  },
+  {
+    date: "Nov 23",
+    2022: 74,
+    2023: null,
+  },
+  {
+    date: "Dec 23",
+    2022: 71,
+    2023: null,
+  },
+];
