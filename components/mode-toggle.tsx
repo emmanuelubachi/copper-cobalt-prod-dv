1edfb9458b0e8afefac13e0c6bfc66bc72932a06
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-06-24 01:14:47 +0100

    build: update nav and mode toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -14,10 +14,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { Toggle } from "@/components/ui/toggle";
+// import { Toggle } from "@/components/ui/toggle";
 
 export default function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  const defaultTheme = "system";
+  const currentTheme = theme || defaultTheme;
 
   return (
     <DropdownMenu>
@@ -27,19 +30,32 @@ export default function ModeToggle() {
           size="icon"
           className="__muted hidden sm:flex"
         >
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <SunIcon
+            className={`h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0`}
+          />
+          <MoonIcon
+            className={`absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100`}
+          />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("light")}
+          className={currentTheme === "light" ? "font-bold" : undefined}
+        >
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("dark")}
+          className={currentTheme === "dark" ? "font-bold" : undefined}
+        >
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem
+          onClick={() => setTheme(defaultTheme)}
+          className={currentTheme === defaultTheme ? "font-bold" : undefined}
+        >
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
