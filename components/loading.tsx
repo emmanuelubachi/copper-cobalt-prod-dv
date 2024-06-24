1950b23e6d83d0f5b88d6a05e9304447029ab6d5
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-06-24 01:13:14 +0100

    build: update loading pages

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Image from "next/image";
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import { CGSPRedSvgIcon } from "./svg/icons/cdsp_logo";
 
-export default function LoadingSpinner() {
+export function LoadingSpinner() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <ClimbingBoxLoader loading={true} color="#36d7b7" />
+    </div>
+  );
+}
+
+export function PageLoadingFallback() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-2">
+      <CGSPRedSvgIcon className="animate-spin-slow h-14 w-14" />
+      <p className="animate-pulse text-p">Loading...</p>
     </div>
   );
 }
