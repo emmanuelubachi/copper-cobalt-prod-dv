822fce34d0d33e6c520c3aa3d72dd26e232feae2
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-06-26 14:42:54 +0100

    build: add store

---
+++ store/markerVisibilityStore.ts
@@ -0,0 +1,19 @@
+import create from "zustand";
+
+interface MarkerVisibilityState {
+  showActiveMarkers: boolean;
+  showInactiveMarkers: boolean;
+  toggleActiveMarkers: () => void;
+  toggleInactiveMarkers: () => void;
+}
+
+const useMarkerVisibilityStore = create<MarkerVisibilityState>((set) => ({
+  showActiveMarkers: false,
+  showInactiveMarkers: false,
+  toggleActiveMarkers: () =>
+    set((state) => ({ showActiveMarkers: !state.showActiveMarkers })),
+  toggleInactiveMarkers: () =>
+    set((state) => ({ showInactiveMarkers: !state.showInactiveMarkers })),
+}));
+
+export default useMarkerVisibilityStore;
