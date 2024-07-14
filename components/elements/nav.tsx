e3e7933528793e2f67bb1d76e927159112f23971
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-07-14 01:00:58 +0100

    style: change mode toggle position

--- components/elements/nav.tsx
+++ components/elements/nav.tsx
@@ -12,46 +12,47 @@ import {
 } from "@/components/ui/tooltip";
 
 import { NAVLIST } from "@/constants/application";
-import Image from "next/image";
 import { CGSPRedSvgIcon } from "../svg/cdsp_logo";
+import ModeToggle from "../mode-toggle";
 
 export default function Nav() {
   const pathname = usePathname();
-  // const { theme, systemTheme } = useTheme();
-
-  // const isDark =
-  //   theme === "dark" || (theme === "system" && systemTheme === "dark");
 
   return (
     <nav className="inset-y-0 left-0 z-10 hidden h-screen w-16 flex-col border-r bg-background sm:sticky sm:flex">
-      <div className={`item-center flex flex-col gap-6 px-4 sm:py-2`}>
-        <div className={`flex h-14 w-full items-center justify-center`}>
-          <Link href="/">
-            <CGSPRedSvgIcon className="h-8 w-8" />
-            <span className="sr-only">China Global South Project</span>
-          </Link>
-        </div>
+      <div className="flex h-full w-full flex-col justify-between">
+        <div className={`item-center flex flex-col gap-6 px-4 sm:py-2`}>
+          <div className={`flex h-14 w-full items-center justify-center`}>
+            <Link href="/">
+              <CGSPRedSvgIcon className="h-8 w-8" />
+              <span className="sr-only">China Global South Project</span>
+            </Link>
+          </div>
 
-        {NAVLIST.map((item) => (
-          <TooltipProvider key={item.name}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href={item.link}
-                  className={`flex h-9 w-9 items-center justify-center rounded-lg p-2 transition-colors hover:text-foreground md:text-sm ${
-                    pathname === item.path
-                      ? "bg-muted text-foreground"
-                      : "text-muted-foreground"
-                  }`}
-                >
-                  {<item.icon />}
-                  <span className="sr-only">{item.name}</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right">{item.name}</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        ))}
+          {NAVLIST.map((item) => (
+            <TooltipProvider key={item.name}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href={item.link}
+                    className={`flex h-9 w-9 items-center justify-center rounded-lg p-2 transition-colors hover:text-foreground md:text-sm ${
+                      pathname === item.path
+                        ? "bg-muted text-foreground"
+                        : "text-muted-foreground"
+                    }`}
+                  >
+                    {<item.icon />}
+                    <span className="sr-only">{item.name}</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right">{item.name}</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          ))}
+        </div>
+        <div className="flex w-full items-center justify-center sm:py-4">
+          <ModeToggle />
+        </div>
       </div>
     </nav>
   );
