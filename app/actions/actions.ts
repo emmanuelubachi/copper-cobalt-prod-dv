4a454a4e76befbee6c4aba98e8d3783a8efaa0ef
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-07-25 00:08:28 +0100

    debug: check path in env

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -1,19 +1,22 @@
 "use server";
-import fs from "fs";
+// import fs from "fs";
+import { promises as fs } from "fs";
+
 import path from "path";
 import { parse } from "csv-parse/sync";
 import process from "process"; // Ensure process is imported
 
 export async function readCsvFile(filename: string) {
   try {
-    const filePath = path.join(
-      process.env.FILE_DIRECTORY || process.cwd(),
-      filename,
-    );
-    console.info(`Current working directory: ${process.cwd()}`);
-    console.info(`Reading file from: ${filePath}`);
+    // const filePath = path.join(
+    //   process.env.FILE_DIRECTORY || process.cwd(),
+    //   filename,
+    // );
 
-    const csvData = await fs.promises.readFile(filePath, "utf8");
+    console.info(`Current working directory: ${process.cwd()}`);
+    // console.info(`Reading file from: ${filePath}`);
+
+    const csvData = await fs.readFile(process.cwd() + "/" + filename, "utf8");
     console.info("File read successfully.");
 
     // Parse CSV data
