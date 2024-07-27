07a3e3d3873803310338db1f8e06f60fc2783b14
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-07-27 22:58:50 +0100

    build: update project page

--- app/(dashboard)/projects/components/project-details.tsx
+++ app/(dashboard)/projects/components/project-details.tsx
@@ -56,11 +56,11 @@ import {
 } from "@/data/chartData";
 
 import { IndustralProjectName } from "@/types";
-import Treemap from "../charts/treeMap";
-import { AreaChartRender } from "../charts/areaChart";
+import Treemap from "@/components/charts/treeMap";
+import { AreaChartRender } from "@/components/charts/areaChart";
 import { Years } from "@/data/chartData";
 
-export default function ProjectSection({
+export default function ProjectDetails({
   projectInfo,
 }: {
   projectInfo: IndustralProjectName;
@@ -336,9 +336,7 @@ export default function ProjectSection({
                     Recent orders from your store.
                   </CardDescription>
                 </CardHeader>
-                <CardContent>
-                  <DataTables />
-                </CardContent>
+                <CardContent>{/* <DataTables /> */}</CardContent>
               </Card>
             </TabsContent>
           </Tabs>
@@ -405,132 +403,132 @@ export default function ProjectSection({
   );
 }
 
-const DataTables = () => {
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Customer</TableHead>
-          <TableHead className="hidden sm:table-cell">Type</TableHead>
-          <TableHead className="hidden sm:table-cell">Status</TableHead>
-          <TableHead className="hidden md:table-cell">Date</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow className="bg-accent">
-          <TableCell>
-            <div className="font-medium">Liam Johnson</div>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              liam@example.com
-            </div>
-          </TableCell>
-          <TableCell className="hidden sm:table-cell">Sale</TableCell>
-          <TableCell className="hidden sm:table-cell">
-            <Badge className="text-xs" variant="secondary">
-              Fulfilled
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
-          <TableCell className="text-right">$250.00</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Olivia Smith</div>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              olivia@example.com
-            </div>
-          </TableCell>
-          <TableCell className="hidden sm:table-cell">Refund</TableCell>
-          <TableCell className="hidden sm:table-cell">
-            <Badge className="text-xs" variant="outline">
-              Declined
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
-          <TableCell className="text-right">$150.00</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Noah Williams</div>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              noah@example.com
-            </div>
-          </TableCell>
-          <TableCell className="hidden sm:table-cell">Subscription</TableCell>
-          <TableCell className="hidden sm:table-cell">
-            <Badge className="text-xs" variant="secondary">
-              Fulfilled
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2023-06-25</TableCell>
-          <TableCell className="text-right">$350.00</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Emma Brown</div>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              emma@example.com
-            </div>
-          </TableCell>
-          <TableCell className="hidden sm:table-cell">Sale</TableCell>
-          <TableCell className="hidden sm:table-cell">
-            <Badge className="text-xs" variant="secondary">
-              Fulfilled
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
-          <TableCell className="text-right">$450.00</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Liam Johnson</div>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              liam@example.com
-            </div>
-          </TableCell>
-          <TableCell className="hidden sm:table-cell">Sale</TableCell>
-          <TableCell className="hidden sm:table-cell">
-            <Badge className="text-xs" variant="secondary">
-              Fulfilled
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
-          <TableCell className="text-right">$250.00</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Olivia Smith</div>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              olivia@example.com
-            </div>
-          </TableCell>
-          <TableCell className="hidden sm:table-cell">Refund</TableCell>
-          <TableCell className="hidden sm:table-cell">
-            <Badge className="text-xs" variant="outline">
-              Declined
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
-          <TableCell className="text-right">$150.00</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <div className="font-medium">Emma Brown</div>
-            <div className="hidden text-sm text-muted-foreground md:inline">
-              emma@example.com
-            </div>
-          </TableCell>
-          <TableCell className="hidden sm:table-cell">Sale</TableCell>
-          <TableCell className="hidden sm:table-cell">
-            <Badge className="text-xs" variant="secondary">
-              Fulfilled
-            </Badge>
-          </TableCell>
-          <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
-          <TableCell className="text-right">$450.00</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
-  );
-};
+// const DataTables = () => {
+//   return (
+//     <Table>
+//       <TableHeader>
+//         <TableRow>
+//           <TableHead>Customer</TableHead>
+//           <TableHead className="hidden sm:table-cell">Type</TableHead>
+//           <TableHead className="hidden sm:table-cell">Status</TableHead>
+//           <TableHead className="hidden md:table-cell">Date</TableHead>
+//           <TableHead className="text-right">Amount</TableHead>
+//         </TableRow>
+//       </TableHeader>
+//       <TableBody>
+//         <TableRow className="bg-accent">
+//           <TableCell>
+//             <div className="font-medium">Liam Johnson</div>
+//             <div className="hidden text-sm text-muted-foreground md:inline">
+//               liam@example.com
+//             </div>
+//           </TableCell>
+//           <TableCell className="hidden sm:table-cell">Sale</TableCell>
+//           <TableCell className="hidden sm:table-cell">
+//             <Badge className="text-xs" variant="secondary">
+//               Fulfilled
+//             </Badge>
+//           </TableCell>
+//           <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+//           <TableCell className="text-right">$250.00</TableCell>
+//         </TableRow>
+//         <TableRow>
+//           <TableCell>
+//             <div className="font-medium">Olivia Smith</div>
+//             <div className="hidden text-sm text-muted-foreground md:inline">
+//               olivia@example.com
+//             </div>
+//           </TableCell>
+//           <TableCell className="hidden sm:table-cell">Refund</TableCell>
+//           <TableCell className="hidden sm:table-cell">
+//             <Badge className="text-xs" variant="outline">
+//               Declined
+//             </Badge>
+//           </TableCell>
+//           <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+//           <TableCell className="text-right">$150.00</TableCell>
+//         </TableRow>
+//         <TableRow>
+//           <TableCell>
+//             <div className="font-medium">Noah Williams</div>
+//             <div className="hidden text-sm text-muted-foreground md:inline">
+//               noah@example.com
+//             </div>
+//           </TableCell>
+//           <TableCell className="hidden sm:table-cell">Subscription</TableCell>
+//           <TableCell className="hidden sm:table-cell">
+//             <Badge className="text-xs" variant="secondary">
+//               Fulfilled
+//             </Badge>
+//           </TableCell>
+//           <TableCell className="hidden md:table-cell">2023-06-25</TableCell>
+//           <TableCell className="text-right">$350.00</TableCell>
+//         </TableRow>
+//         <TableRow>
+//           <TableCell>
+//             <div className="font-medium">Emma Brown</div>
+//             <div className="hidden text-sm text-muted-foreground md:inline">
+//               emma@example.com
+//             </div>
+//           </TableCell>
+//           <TableCell className="hidden sm:table-cell">Sale</TableCell>
+//           <TableCell className="hidden sm:table-cell">
+//             <Badge className="text-xs" variant="secondary">
+//               Fulfilled
+//             </Badge>
+//           </TableCell>
+//           <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+//           <TableCell className="text-right">$450.00</TableCell>
+//         </TableRow>
+//         <TableRow>
+//           <TableCell>
+//             <div className="font-medium">Liam Johnson</div>
+//             <div className="hidden text-sm text-muted-foreground md:inline">
+//               liam@example.com
+//             </div>
+//           </TableCell>
+//           <TableCell className="hidden sm:table-cell">Sale</TableCell>
+//           <TableCell className="hidden sm:table-cell">
+//             <Badge className="text-xs" variant="secondary">
+//               Fulfilled
+//             </Badge>
+//           </TableCell>
+//           <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+//           <TableCell className="text-right">$250.00</TableCell>
+//         </TableRow>
+//         <TableRow>
+//           <TableCell>
+//             <div className="font-medium">Olivia Smith</div>
+//             <div className="hidden text-sm text-muted-foreground md:inline">
+//               olivia@example.com
+//             </div>
+//           </TableCell>
+//           <TableCell className="hidden sm:table-cell">Refund</TableCell>
+//           <TableCell className="hidden sm:table-cell">
+//             <Badge className="text-xs" variant="outline">
+//               Declined
+//             </Badge>
+//           </TableCell>
+//           <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+//           <TableCell className="text-right">$150.00</TableCell>
+//         </TableRow>
+//         <TableRow>
+//           <TableCell>
+//             <div className="font-medium">Emma Brown</div>
+//             <div className="hidden text-sm text-muted-foreground md:inline">
+//               emma@example.com
+//             </div>
+//           </TableCell>
+//           <TableCell className="hidden sm:table-cell">Sale</TableCell>
+//           <TableCell className="hidden sm:table-cell">
+//             <Badge className="text-xs" variant="secondary">
+//               Fulfilled
+//             </Badge>
+//           </TableCell>
+//           <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+//           <TableCell className="text-right">$450.00</TableCell>
+//         </TableRow>
+//       </TableBody>
+//     </Table>
+//   );
+// };
