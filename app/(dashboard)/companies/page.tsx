b8b625c4cdf9347d69120d1de1bea6395aa366c7
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-07-28 17:25:56 +0100

    build: update companies page

--- app/(dashboard)/companies/page.tsx
+++ app/(dashboard)/companies/page.tsx
@@ -4,10 +4,10 @@ import GridList from "@/components/sections/gridList";
 
 export default function Page() {
   return (
-    <main className="mb-24 mt-10 flex-col space-y-8 p-4 sm:mb-20 sm:mt-16 sm:px-6 sm:py-4">
-      <div className="flex">
+    <main className="mb-24 mt-4 flex-col space-y-8 p-4 sm:mb-20 sm:mt-4 sm:px-6 sm:py-4">
+      {/* <div className="flex">
         <BackButton />
-      </div>
+      </div> */}
       <GridList />
     </main>
   );
