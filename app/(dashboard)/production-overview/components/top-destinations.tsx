4dc821d0f6ec6c21a7880c2263b482e602bb7956
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-08-12 03:43:18 +0100

    update: top destination chart and year

--- app/(dashboard)/production-overview/components/top-destinations.tsx
+++ app/(dashboard)/production-overview/components/top-destinations.tsx
@@ -1,26 +1,50 @@
 import CustomLabelBarChart from "@/components/charts/shadcn/bar-chart/custom-label-bar-chart";
-import { coDestSumChartConfig, cuDestSumChartConfig } from "@/constants/chart";
+// import { coDestSumChartConfig, cuDestSumChartConfig } from "@/constants/chart";
+const coDestSumChartConfig = {
+  quantity: {
+    label: `Qty (T) ${" "}`,
+    color: "hsl(var(--chart-6))",
+  },
+  label: {
+    color: "hsl(var(--background))",
+  },
+};
 
-export default function TopDestinations({ coDestSum, cuDestSum }: any) {
+const cuDestSumChartConfig = {
+  quantity: {
+    label: `Qty (T) ${" "}`,
+    color: "hsl(var(--chart-5))",
+  },
+  label: {
+    color: "hsl(var(--background))",
+  },
+};
+
+export default function TopDestinations({
+  selectedYear,
+  coDestSum,
+  cuDestSum,
+}: any) {
+  const year = selectedYear;
   return (
     <section className="grid items-start gap-2 xl:col-span-2">
       <div className="grid gap-4 lg:grid-cols-2">
         <CustomLabelBarChart
-          title="Top Destinations of Cobalt Production in 2023"
+          title={`Top Destinations of Cobalt Production in ${year}`}
           description="Quantity in Tonnes"
           config={coDestSumChartConfig}
           chartData={coDestSum}
-          yAxisDataKey="destination"
-          xAxisDataKey="totalQuantityTons"
+          yAxisDataKey="short_destination"
+          xAxisDataKey="quantity"
         />
 
         <CustomLabelBarChart
-          title="Top Destinations of Copper Production in 2023"
+          title={`Top Destinations of Copper Production in ${year}`}
           description="Quantity in Tonnes"
           config={cuDestSumChartConfig}
           chartData={cuDestSum}
-          yAxisDataKey="destination"
-          xAxisDataKey="totalQuantityTons"
+          yAxisDataKey="short_destination"
+          xAxisDataKey="quantity"
         />
       </div>
     </section>
