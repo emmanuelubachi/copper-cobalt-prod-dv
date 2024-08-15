e104af2afb87c34c2e2307ce0c081322dfb43afb
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-08-15 13:44:46 +0100

    build: update Companies

--- app/(dashboard)/companies/page.tsx
+++ app/(dashboard)/companies/page.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import BackButton from "@/components/elements/backButton";
-import GridList from "@/components/sections/gridList";
+import GridList from "@/app/(dashboard)/companies/components/gridList";
+import { IndustralProjectsNode } from "@/constants/application";
 
 export default function Page() {
   return (
     <main className="mb-24 mt-0 flex-col space-y-4 p-4 sm:mb-20 sm:mt-0 sm:px-6 sm:py-3">
       <header className="sm:ml-1">
-        <h1 className="text-h4 font-medium tracking-tight">Select a project</h1>
+        {/* <h1 className="text-h4 font-medium tracking-tight">Select a project</h1> */}
       </header>
 
       {/* <div className="flex">
         <BackButton />
       </div> */}
-      <GridList />
+      <GridList data={IndustralProjectsNode} />
     </main>
   );
 }
