1f1d144b3358deff0e47344fa8d701617bf9dd52
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-08-18 00:27:23 +0100

    style: update gridList padding

--- app/(dashboard)/companies/page.tsx
+++ app/(dashboard)/companies/page.tsx
@@ -5,8 +5,8 @@ import { CompaniesList } from "@/constants/application";
 
 export default function Page() {
   return (
-    <main className="mb-24 mt-0 flex-col space-y-4 p-4 sm:mb-20 sm:mt-0 sm:px-6 sm:py-3">
-      <header className="rounded-lg bg-background px-4 py-6 dark:bg-neutral-900">
+    <main className="mb-24 mt-0 flex-col space-y-4 p-4 sm:mb-20 sm:mt-0 sm:px-8 sm:py-3">
+      <header className="rounded-lg bg-background py-4 dark:bg-neutral-900">
         <h1 className="text-center text-h4 font-medium tracking-tight lg:text-start lg:text-h5 xl:text-h4">
           Mining Companies in the Democratic Republic of the Congo
         </h1>
