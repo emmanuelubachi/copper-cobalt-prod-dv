1ac92d91794fdbc9ecf95fe1167a5a2c06e26dfb
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-08-17 23:46:01 +0100

    style: update companies page heading position

--- app/(dashboard)/companies/page.tsx
+++ app/(dashboard)/companies/page.tsx
@@ -1,22 +1,13 @@
 import React from "react";
-// import {  } from "lucide-react";
 
-import BackButton from "@/components/elements/backButton";
 import GridList from "@/app/(dashboard)/companies/components/gridList";
 import { CompaniesList } from "@/constants/application";
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
     <main className="mb-24 mt-0 flex-col space-y-4 p-4 sm:mb-20 sm:mt-0 sm:px-6 sm:py-3">
       <header className="rounded-lg bg-background px-4 py-6 dark:bg-neutral-900">
-        <h1 className="text-h4 font-medium tracking-tight">
+        <h1 className="text-center text-h4 font-medium tracking-tight lg:text-start lg:text-h5 xl:text-h4">
           Mining Companies in the Democratic Republic of the Congo
         </h1>
       </header>
