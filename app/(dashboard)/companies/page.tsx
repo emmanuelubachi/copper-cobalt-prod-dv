12a1ba13c1ff6d81a544db0edd17b7b981e276af
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-08-23 13:41:37 +0100

    style: update headers

--- app/(dashboard)/companies/page.tsx
+++ app/(dashboard)/companies/page.tsx
@@ -5,9 +5,9 @@ import { CompaniesList } from "@/constants/application";
 
 export default function Page() {
   return (
-    <main className="mb-24 mt-0 flex-col space-y-4 p-4 sm:mb-20 sm:mt-0 sm:px-8 sm:py-3">
-      <header className="rounded-lg bg-background py-4 dark:bg-neutral-900">
-        <h1 className="text-center text-h4 font-medium tracking-tight lg:text-start lg:text-h5 xl:text-h4">
+    <main className="mb-24 mt-0 flex-col space-y-4 px-4 sm:mb-20 sm:mt-0 sm:px-8">
+      <header className="left-0 right-0 z-20 bg-white py-4 dark:bg-neutral-900">
+        <h1 className="text-center text-h5 font-medium tracking-tight lg:text-start lg:text-h5 xl:text-h5">
           Mining Companies in the Democratic Republic of the Congo
         </h1>
       </header>
