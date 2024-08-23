88edf016ebbf70352bdda68109301d75e61d493e
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-08-23 19:00:29 +0100

    build: update code

--- app/(dashboard)/projects/components/project-details.tsx
+++ app/(dashboard)/projects/components/project-details.tsx
@@ -146,139 +146,173 @@ export default function ProjectDetails({
   }, [project_id]);
 
   return (
-    <section className="space-y-4">
-      <div className="left-0 right-0 z-20 items-center justify-between gap-6 space-y-4 bg-background/50 py-4 backdrop-blur-md dark:bg-neutral-900/50 lg:flex lg:space-y-0">
-        <h2 className="text-center text-h5 font-medium tracking-tight lg:text-start">
-          {projectInfo.project_name}
-        </h2>
-        <div className="flex items-center justify-center gap-2 sm:items-start lg:justify-end">
+    <section className="space-y-2">
+      <div className="left-0 right-0 z-20 items-center justify-end gap-6 space-y-4 bg-background/50 py-4 backdrop-blur-md dark:bg-neutral-900/50 lg:flex lg:space-y-0">
+        <div className="flex items-center justify-end gap-2">
           <YearToggle
             defaultValue={selectedYear}
             onChangeFunction={setSelectedYear}
             years={Years}
           />
-          {/* <div>
-            <FilterButton
-              content={<ProjectFilter />}
-              label="Projects"
-              type="tooltip"
-              tooltip="Select a project"
-              className=""
-            />
-          </div> */}
         </div>
       </div>
-      <div className="grid flex-1 items-start gap-4 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-        <div className="grid auto-rows-max items-start gap-4 md:gap-6 lg:col-span-3">
-          <div className="grid gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3">
+
+      {/* Charts */}
+      <div className="space-y-4">
+        <div className="grid items-start gap-4 xl:grid-cols-3">
+          {/* Total production */}
+          <div className="space-y-4 xl:col-span-2">
             {totalProd.length > 0 && (
-              <Card className="__card">
-                <CardHeader>
-                  <CardTitle>
-                    {totalProd.length > 0 && totalProd[0].year} Annual
-                    Production
+              <Card className="shrink border-none bg-transparent shadow-none lg:col-span-2 lg:h-fit">
+                <CardContent className="space-y-4 px-0 lg:pt-2">
+                  <CardTitle className="text-start text-h5 font-medium tracking-tight">
+                    {projectInfo.project_name}
                   </CardTitle>
-                  <CardDescription>Quantity in Tonnes</CardDescription>
-                </CardHeader>
-                <Separator />
-                <CardContent className="mt-2 lg:h-[330px]">
-                  <div className="flex flex-col gap-7">
-                    {totalProd[0].totalCobalt > 0 && (
-                      <div className="space-y-5">
-                        <div className="font-bold">
-                          <p className="text-h4 font-bold text-chart6">
-                            {totalProd[0].totalCobalt > 0 &&
-                              totalProd[0].totalCobalt
-                                .toFixed(1)
-                                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                            {" t"}
-                          </p>
-                          <span className="text-sm text-foreground/50">
-                            {" "}
-                            Total Cobalt Production
-                          </span>
-                        </div>
 
-                        <div className="flex items-center justify-between pr-8">
-                          {totalProdDetails[0].totalCobaltExport > 0 && (
-                            <div className="font-bold">
-                              <p className="text-p font-bold text-foreground/80">
-                                {totalProdDetails[0].totalCobaltExport
-                                  .toFixed(1)
-                                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " t"}
-                              </p>
-                              <span className="text-pxs text-foreground/50">
-                                Co Exports
-                              </span>
-                            </div>
-                          )}
-                          {totalProdDetails[0].totalCobaltLocal > 0 && (
-                            <div className="font-bold">
-                              <p className="text-p font-bold text-foreground/80">
-                                {totalProdDetails[0].totalCobaltLocal
-
-                                  .toFixed(1)
-                                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " t"}
-                              </p>
-                              <span className="text-pxs font-bold text-foreground/50">
-                                Co Domestic Sales
-                              </span>
-                            </div>
-                          )}
-                        </div>
+                  <div className="grid gap-2 lg:grid-cols-2">
+                    <h3 className="text-start text-p font-medium tracking-tight">
+                      {projectInfo.project_name}
+                    </h3>
+                    <div className="space-y-2">
+                      <div>
+                        <CardTitle>
+                          {totalProd.length > 0 && totalProd[0].year} Annual
+                          Production
+                        </CardTitle>
+                        <CardDescription>
+                          Total Production including quantity sold locally and
+                          exported
+                        </CardDescription>
                       </div>
-                    )}
-                    {totalProd[0].totalCopper > 0 && (
-                      <div className="space-y-5">
-                        <div className="font-bold">
-                          <p className="text-h4 font-bold text-chart5">
-                            {totalProd[0].totalCopper > 0 &&
-                              totalProd[0].totalCopper
-                                .toFixed(1)
-                                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                            {" t"}
-                          </p>
-                          <span className="text-sm text-foreground/50">
-                            {" "}
-                            Total Copper Production
-                          </span>
-                        </div>
 
-                        <div className="flex items-center justify-between pr-8">
-                          {totalProdDetails[0].totalCopperExport > 0 && (
-                            <div className="font-bold">
-                              <p className="text-p font-bold text-foreground/80">
-                                {totalProdDetails[0].totalCopperExport
-                                  .toFixed(1)
-                                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " t"}
+                      <div className="flex gap-10">
+                        {totalProd[0].totalCobalt > 0 && (
+                          <div className="space-y-4">
+                            <div className="border-l-4 border-chart6 pl-4 font-bold">
+                              <p className="text-h5 font-bold text-foreground/90">
+                                {totalProd[0].totalCobalt > 0 &&
+                                  totalProd[0].totalCobalt
+                                    .toFixed(1)
+                                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                                {" t"}
                               </p>
-                              <span className="text-pxs text-foreground/50">
-                                Cu Exports
-                              </span>
-                            </div>
-                          )}
-                          {totalProdDetails[0].totalCopperLocal > 0 && (
-                            <div className="font-bold">
-                              <p className="text-p font-bold text-foreground/80">
-                                {totalProdDetails[0].totalCopperLocal
-                                  .toFixed(1)
-                                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " t"}
+                              <p className="text-sm text-foreground/70">
+                                <span className="text-chart6">Cobalt</span>{" "}
+                                Production
                               </p>
-                              <span className="text-pxs font-bold text-foreground/50">
-                                Cu Domestic Sales
-                              </span>
                             </div>
-                          )}
-                        </div>
+
+                            {/* <div className="flex items-center justify-between pr-8">
+                              {totalProdDetails[0].totalCobaltExport > 0 && (
+                                <div className="font-bold">
+                                  <p className="text-p font-bold text-foreground/80">
+                                    {totalProdDetails[0].totalCobaltExport
+                                      .toFixed(1)
+                                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
+                                      " t"}
+                                  </p>
+                                  <span className="text-pxs text-foreground/50">
+                                    Co Exports
+                                  </span>
+                                </div>
+                              )}
+                              {totalProdDetails[0].totalCobaltLocal > 0 && (
+                                <div className="font-bold">
+                                  <p className="text-p font-bold text-foreground/80">
+                                    {totalProdDetails[0].totalCobaltLocal
+
+                                      .toFixed(1)
+                                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
+                                      " t"}
+                                  </p>
+                                  <span className="text-pxs font-bold text-foreground/50">
+                                    Co Domestic Sales
+                                  </span>
+                                </div>
+                              )}
+                            </div> */}
+                          </div>
+                        )}
+                        {totalProd[0].totalCopper > 0 && (
+                          <div className="space-y-4">
+                            <div className="border-l-4 border-chart5 pl-4 font-bold">
+                              <p className="text-h5 font-bold text-foreground/90">
+                                {totalProd[0].totalCopper > 0 &&
+                                  totalProd[0].totalCopper
+                                    .toFixed(1)
+                                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                                {" t"}
+                              </p>
+                              <p className="text-sm text-foreground/70">
+                                <span className="text-chart5">Copper</span>{" "}
+                                Production
+                              </p>
+                            </div>
+
+                            {/* <div className="flex items-center justify-between pr-8">
+                              {totalProdDetails[0].totalCopperExport > 0 && (
+                                <div className="font-bold">
+                                  <p className="text-p font-bold text-foreground/80">
+                                    {totalProdDetails[0].totalCopperExport
+                                      .toFixed(1)
+                                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
+                                      " t"}
+                                  </p>
+                                  <span className="text-pxs text-foreground/50">
+                                    Cu Exports
+                                  </span>
+                                </div>
+                              )}
+                              {totalProdDetails[0].totalCopperLocal > 0 && (
+                                <div className="font-bold">
+                                  <p className="text-p font-bold text-foreground/80">
+                                    {totalProdDetails[0].totalCopperLocal
+                                      .toFixed(1)
+                                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
+                                      " t"}
+                                  </p>
+                                  <span className="text-pxs font-bold text-foreground/50">
+                                    Cu Domestic Sales
+                                  </span>
+                                </div>
+                              )}
+                            </div> */}
+                          </div>
+                        )}
                       </div>
-                    )}
+                    </div>
                   </div>
                 </CardContent>
               </Card>
             )}
 
+            {/* Destination */}
+            <div className="grid">
+              {coDestinationData.length > 0 && (
+                <CustomLabelBarChart
+                  title="Top Destinations of Cobalt Production in 2023"
+                  description="Quantity in Tonnes"
+                  config={coDestChartConfig}
+                  chartData={coDestinationData}
+                  yAxisDataKey="destination"
+                  xAxisDataKey="quantity_tons"
+                  footNote={
+                    <>
+                      <div className="leading-none text-muted-foreground">
+                        Showing top {coDestinationData.length > 4 ? 5 : ""}{" "}
+                        destinations in 2023.
+                      </div>
+                    </>
+                  }
+                />
+              )}
+            </div>
+          </div>
+
+          {/* Monthly Production */}
+          <div className="grid grid-cols-1 gap-4">
             {monthlyData.length > 0 && (
-              <div className="shrink lg:col-span-2">
+              <div className="">
                 <MultipleBarChart
                   title="Production of Copper and Cobalt in 2023"
                   description="Quantity in Tonnes"
@@ -286,6 +320,25 @@ export default function ProjectDetails({
                   chartData={monthlyData}
                   firstDataKey="Cobalt"
                   secondDataKey="Copper"
+                  classname="h-[189px]"
+                  footNote={
+                    <div className="leading-none text-muted-foreground">
+                      Includes quantities both exported and sold locally.
+                    </div>
+                  }
+                />
+              </div>
+            )}
+            {monthlyData.length > 0 && (
+              <div className="">
+                <MultipleBarChart
+                  title="Production of Copper and Cobalt in 2023"
+                  description="Quantity in Tonnes"
+                  config={monthlyProdChartConfig}
+                  chartData={monthlyData}
+                  firstDataKey="Cobalt"
+                  secondDataKey="Copper"
+                  classname="h-[189px]"
                   footNote={
                     <div className="leading-none text-muted-foreground">
                       Includes quantities both exported and sold locally.
@@ -295,47 +348,47 @@ export default function ProjectDetails({
               </div>
             )}
           </div>
-
-          <div className="grid gap-4 md:gap-6 lg:grid-cols-2 xl:grid-cols-2">
-            {coDestinationData.length > 0 && (
-              <CustomLabelBarChart
-                title="Top Destinations of Cobalt Production in 2023"
-                description="Quantity in Tonnes"
-                config={coDestChartConfig}
-                chartData={coDestinationData}
-                yAxisDataKey="destination"
-                xAxisDataKey="quantity_tons"
-                footNote={
-                  <>
-                    <div className="leading-none text-muted-foreground">
-                      Showing top {coDestinationData.length > 4 ? 5 : ""}{" "}
-                      destinations in 2023.
-                    </div>
-                  </>
-                }
-              />
-            )}
-
-            {cuDestinationData.length > 0 && (
-              <CustomLabelBarChart
-                title="Top Destinations of Copper Production in 2023"
-                description="Quantity in Tonnes"
-                config={cuDestChartConfig}
-                chartData={cuDestinationData}
-                yAxisDataKey="destination"
-                xAxisDataKey="quantity_tons"
-                footNote={
-                  <>
-                    <div className="leading-none text-muted-foreground">
-                      Showing top {cuDestinationData.length > 4 ? 5 : ""}{" "}
-                      destinations in 2023.
-                    </div>
-                  </>
-                }
-              />
-            )}
-          </div>
         </div>
+        {/* Destination */}
+        {/* <div className="grid gap-4 lg:grid-cols-2 xl:grid-cols-2">
+          {coDestinationData.length > 0 && (
+            <CustomLabelBarChart
+              title="Top Destinations of Cobalt Production in 2023"
+              description="Quantity in Tonnes"
+              config={coDestChartConfig}
+              chartData={coDestinationData}
+              yAxisDataKey="destination"
+              xAxisDataKey="quantity_tons"
+              footNote={
+                <>
+                  <div className="leading-none text-muted-foreground">
+                    Showing top {coDestinationData.length > 4 ? 5 : ""}{" "}
+                    destinations in 2023.
+                  </div>
+                </>
+              }
+            />
+          )}
+
+          {cuDestinationData.length > 0 && (
+            <CustomLabelBarChart
+              title="Top Destinations of Copper Production in 2023"
+              description="Quantity in Tonnes"
+              config={cuDestChartConfig}
+              chartData={cuDestinationData}
+              yAxisDataKey="destination"
+              xAxisDataKey="quantity_tons"
+              footNote={
+                <>
+                  <div className="leading-none text-muted-foreground">
+                    Showing top {cuDestinationData.length > 4 ? 5 : ""}{" "}
+                    destinations in 2023.
+                  </div>
+                </>
+              }
+            />
+          )}
+        </div> */}
       </div>
     </section>
   );
