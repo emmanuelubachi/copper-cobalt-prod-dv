7ace236f71494d61df71c059fb3c3bfeb86d1af4
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-08-31 05:20:24 +0100

    build: add monthly export data and update chart

--- components/charts/shadcn/bar-chart/multiple-bar-chart.tsx
+++ components/charts/shadcn/bar-chart/multiple-bar-chart.tsx
@@ -26,7 +26,7 @@ type MultipleBarChartProps = {
   config: ChartConfig;
   chartData: any;
   firstDataKey: string;
-  secondDataKey: string;
+  secondDataKey?: string;
   footNote?: ReactNode;
   classname?: string;
 };
@@ -67,8 +67,55 @@ export default function MultipleBarChart({ ...props }: MultipleBarChartProps) {
               radius={4}
             />
             <Bar
-              dataKey={props.secondDataKey}
+              dataKey={props.secondDataKey || props.firstDataKey}
               fill={`var(--color-${props.secondDataKey}`}
+              radius={4}
+            />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      {props.footNote && (
+        <CardFooter className="flex-col items-start gap-2 text-sm">
+          {props.footNote}
+        </CardFooter>
+      )}
+    </Card>
+  );
+}
+
+export function MultipleBarChart2({ ...props }: MultipleBarChartProps) {
+  const chartConfig = props.config satisfies ChartConfig;
+
+  const chartData = props.chartData;
+
+  return (
+    <Card className="__card">
+      <CardHeader className="border-b px-4">
+        <CardTitle>{props.title}</CardTitle>
+        <CardDescription>{props.description}</CardDescription>
+      </CardHeader>
+      <CardContent className="px-4 pt-4 sm:px-6 sm:pt-6">
+        <ChartContainer
+          config={chartConfig}
+          className={cn("aspect-auto h-[250px] w-full", props.classname)}
+        >
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              // tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="dashed" />}
+            />
+            <ChartLegend content={<ChartLegendContent />} />
+            <Bar
+              dataKey={props.firstDataKey}
+              fill={`var(--color-${props.firstDataKey}`}
               radius={4}
             />
           </BarChart>
