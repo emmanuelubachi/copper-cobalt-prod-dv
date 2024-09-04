bfa8158a0b3dfbcc00b2c26e4533c0e2baac65de
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2024-09-04 23:43:14 +0100

    build: add share button

--- app/(dashboard)/companies/page.tsx
+++ app/(dashboard)/companies/page.tsx
@@ -2,14 +2,18 @@ import React from "react";
 
 import GridList from "@/app/(dashboard)/companies/components/gridList";
 import { CompaniesList } from "@/constants/application";
+import { ShareButton } from "@/components/elements/shareButton";
 
 export default function Page() {
   return (
     <main>
-      <header className="left-0 right-0 z-20 bg-white p-4 dark:bg-neutral-900 sm:px-8">
-        <h1 className="text-center text-h5 font-medium tracking-tight lg:text-start lg:text-h5 xl:text-h5">
+      <header className="left-0 right-0 z-20 flex items-start justify-between gap-4 bg-white p-4 dark:bg-neutral-900 sm:px-8">
+        <h1 className="text-start text-h5 font-medium tracking-tight lg:text-start lg:text-h5 xl:text-h5">
           Mining Companies in the Democratic Republic of the Congo
         </h1>
+        <div className="flex justify-end">
+          <ShareButton />
+        </div>
       </header>
 
       <section className="mb-24 mt-0 flex-col space-y-4 px-4 sm:mb-20 sm:mt-0 sm:px-8">
